f29797dd172a4e1fb66b9949797bdb423fdce9c2
theMortezaHH <mortezahh2022@gmail.com>
2025-06-13 21:31:23 +0330

    Updated .env files and build script in package.json

--- Server/src/app.module.ts
+++ Server/src/app.module.ts
@@ -2,12 +2,21 @@ import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
 import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
+import { ThrottlerModule } from "@nestjs/throttler"
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			envFilePath: ".env",
 			isGlobal: true,
+		}),
+		ThrottlerModule.forRoot({
+			throttlers: [
+				{
+					limit: Number(process.env.THROTTLE_LIMIT),
+					ttl: Number(process.env.THROTTLE_TTL),
+				},
+			],
 		}),
 	],
 	controllers: [AppController],
--- Server/src/main.ts
+++ Server/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
+import helmet from "helmet"
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+
+	app.use(helmet())
+	app.setGlobalPrefix("api")
+
 	await app.listen(process.env.PORT || 3000)
 }
 bootstrap()
